2383672c0b086f0c1e3e0a513903b353348722ac
SSAFY 7기 전병찬 <81104902+blackbox98@users.noreply.github.com>
2023-04-12 22:50:44 +0900

    Add files via upload

---
+++ NURI-BOM/src/test/java/com/autonomous/nuribom/NuriBomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autonomous.nuribom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuriBomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
